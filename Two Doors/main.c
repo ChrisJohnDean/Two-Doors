6f8ceb6bc5658f82c1b636b2941d09dabe77197c
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-01-30 20:11:49 -0800

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Chris Dean on 2018-01-30.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
